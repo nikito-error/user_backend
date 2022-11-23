01c23b895a510a71e752fde6fe6e43046c568fd0
nikito-error <nisanx@abv.bg>
2022-11-23 15:27:24 +0200

    add crossorigin
    tt

--- src/main/java/com/rest/controller/UserController.java
+++ src/main/java/com/rest/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.rest.model.User;
 import com.rest.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/users")
 public class UserController {
 	private final UserService userService;
